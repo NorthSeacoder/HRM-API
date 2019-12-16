de9de623d6dcc3aae892b866b1d5e9cb0c773165
mengpeng <pengmeng@yangqianguan.com>
2019-12-16 18:21:59 +0800

    [HRM-API][delete] some words

--- models/employee.js
+++ models/employee.js
@@ -4,6 +4,5 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 var EmployeeSchema = new Schema({})
 var Employee = mongoose.model('Employee', EmployeeSchema)
-// console.log('我是你爸爸')
 
 module.exports = Employee
